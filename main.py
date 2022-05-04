34e7934baf28de90a67aafe44edbd09f187e873e
JaneMusimbi <janemusimbij@gmail.com>
2022-05-04 05:41:30 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+count = 0
+for number in range(1,10):
+    if number % 2 ==0:
+        count +=1
+        print(number)
+print(f"we have{count} even numbers")
+
+Fruits =["Apples","Banana","cherry"]
+for x in Fruits:
+     print(x)
+     x in Fruits[0]
+     print(x)
+     x in Fruits[1]
+     print(x)
+     x in Fruits[2]
+     print(x)
+a=5
+while a<10:
+   print("a is less than 10")
+   break
+   a +=5
+
+m = 0
+while m< 6:
+  m += 1
+  if m == 3:
+    
+    continue
+
+  print(m)
+
